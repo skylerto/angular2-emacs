aa82996bee047671e80b1f68d21a928c0fa5ffc2
skylerl <skylerl@indellient.com>
2016-12-14 10:53:58 -0500

    Use local variables

--- angular2.el
+++ angular2.el
@@ -1,16 +1,16 @@
 ;; Angular2 Jump to Template
 (defun jump-to-template ()
-  (defvar jump-to-template-files
-	(split-string (buffer-file-name) "\\."))
+
+  (let (filename)
+    (setq filename (split-string (buffer-file-name) "\\."))
   (find-file
-   (concat (elt jump-to-template-files 0) "." (elt jump-to-template-files 1) ".html")))
+   (concat (elt filename 0) "." (elt filename 1) ".html"))))
 (global-set-key (kbd "C-c C-t") (lambda () (interactive) (jump-to-template)))
 
 ;; Angular2 Jump to Component.
 (defun jump-to-component()
-  (defvar jump-to-component-files
-	(split-string (buffer-file-name) "\\."))
+  (let (filename)
+    (setq filename (split-string (buffer-file-name) "\\."))
   (find-file
-   (concat
-    (elt jump-to-component-files 0) "." (elt jump-to-component-files 1) ".ts")))
+   (concat (elt filename 0) "." (elt filename 1) ".ts"))))
 (global-set-key (kbd "C-c C-c") (lambda () (interactive) (jump-to-component)))
